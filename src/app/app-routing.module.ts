f64c390e4a1a685fd72bd6249ac9cd92e34da23d
ChristianDBrooks <christiandbrooks@gmail.com>
2019-08-14 17:03:17 -0400

    Added routing, started home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { RegistryPageComponent } from './registry-page/registry-page.component';
+import { GalleryPageComponent } from './gallery-page/gallery-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:  '', redirectTo:  'home', pathMatch:  'full' },
+  {
+    path: 'home',
+    component: HomePageComponent
+  },
+  {
+    path: 'registry',
+    component: RegistryPageComponent
+  },
+  {
+    path: 'gallery',
+    component: GalleryPageComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { RegistryPageComponent } from './registry-page/registry-page.component';
+import { GalleryPageComponent } from './gallery-page/gallery-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    RegistryPageComponent,
+    GalleryPageComponent
   ],
   imports: [
     BrowserModule,
